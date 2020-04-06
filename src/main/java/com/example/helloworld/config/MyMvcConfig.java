1a815053747eb2ed3e6db70209771b631ff7646b
T-yuhs <565826475@qq.com>
2020-04-06 17:50:08 +0800

    springboot01

---
+++ src/main/java/com/example/helloworld/config/MyMvcConfig.java
@@ -0,0 +1,7 @@
+package com.example.helloworld.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyMvcConfig {
+}
---
+++ src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
